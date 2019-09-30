1dfc345f5412b3481059fe2e0f7db63d24af1331
educanavarro <educanavarro@users.noreply.github.com>
2019-09-29 23:22:46 -0300

    Atividade da faculdade

---
+++ src/poupex/Poupex.java
@@ -0,0 +1,11 @@
+package poupex;
+import telas.Principal;
+
+public class Poupex {
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando Sistema...");
+        new Principal().setVisible(true);
+    }
+    
+}
---
+++ src/telas/Principal.java
@@ -0,0 +1,136 @@
+package telas;
+
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
+
+public class Principal extends javax.swing.JFrame {
+
+    public Principal() {
+        initComponents();
+    }
+    private double juros;
+    private int mes;
+    private double deposito;
+    private double rendimento;
+    private double total;
+    
+    DecimalFormat formato = new DecimalFormat("#.##");
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        lbJurosMes = new javax.swing.JLabel();
+        lbNumAnos = new javax.swing.JLabel();
+        lbDeposMens = new javax.swing.JLabel();
+        lbTotalPoupado = new javax.swing.JLabel();
+        txtJurosMes = new javax.swing.JTextField();
+        txtNumAnos = new javax.swing.JTextField();
+        txtDeposMens = new javax.swing.JTextField();
+        btOk = new javax.swing.JButton();
+        lbPoupado = new javax.swing.JLabel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Poupex");
+
+        lbJurosMes.setText("Juros ao mês %:");
+
+        lbNumAnos.setText("Num. de anos:");
+
+        lbDeposMens.setText("Depósito mensal R$:");
+
+        lbTotalPoupado.setText("Total poupado R$:");
+
+        btOk.setText("OK");
+        btOk.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btOkActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lbJurosMes)
+                            .addComponent(lbNumAnos)
+                            .addComponent(lbDeposMens))
+                        .addGap(34, 34, 34))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(lbTotalPoupado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(txtDeposMens)
+                    .addComponent(txtNumAnos)
+                    .addComponent(txtJurosMes)
+                    .addComponent(btOk, javax.swing.GroupLayout.DEFAULT_SIZE, 121, Short.MAX_VALUE)
+                    .addComponent(lbPoupado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbJurosMes)
+                    .addComponent(txtJurosMes, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbNumAnos)
+                    .addComponent(txtNumAnos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lbDeposMens)
+                    .addComponent(txtDeposMens, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(14, 14, 14)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(lbTotalPoupado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(lbPoupado, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(11, 11, 11)
+                .addComponent(btOk)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btOkActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btOkActionPerformed
+        // TODO add your handling code here:
+        mes = Integer.parseInt(txtNumAnos.getText())*12;
+        juros = Double.parseDouble(txtJurosMes.getText());
+        deposito = Double.parseDouble(txtDeposMens.getText());
+        total = 0;
+        for (int r=0; r<mes; r++){
+            rendimento = ((total)*juros)/ 100;    
+            total += rendimento + deposito;    
+    }
+        lbPoupado.setText(String.valueOf(formato.format(total)));
+        
+    }//GEN-LAST:event_btOkActionPerformed
+
+
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Principal().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btOk;
+    private javax.swing.JLabel lbDeposMens;
+    private javax.swing.JLabel lbJurosMes;
+    private javax.swing.JLabel lbNumAnos;
+    private javax.swing.JLabel lbPoupado;
+    private javax.swing.JLabel lbTotalPoupado;
+    private javax.swing.JTextField txtDeposMens;
+    private javax.swing.JTextField txtJurosMes;
+    private javax.swing.JTextField txtNumAnos;
+    // End of variables declaration//GEN-END:variables
+}
